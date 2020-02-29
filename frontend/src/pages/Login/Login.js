57841a80d08193ee0850306590d76d86ed26ba56
bdriguesdev <bdriguesdev@gmail.com>
2020-02-29 16:01:12 -0300

    First commit

---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    <main className="login">
+        <h1>Agenda Churras</h1>
+        <form>
+
+        </form>
+    </main>
+};
+
+export default Login;
