682781aeace721afe28d12fe68208fc8c01ecfb6
bdriguesdev <bdriguesdev@gmail.com>
2020-03-09 01:30:08 -0300

    Backend: Now the path to the build is correct.

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,10 +22,10 @@ app.use('/churrasco', churrascoRoutes);
 app.use('/participant', participantRoutes);
 
 //react app
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
 });
 
 //db
